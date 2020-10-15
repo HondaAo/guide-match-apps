c980e04c917941a06d974f7d21e94cdbf06bd75f
Honda <hondao1207@gmail.com>
2020-10-16 00:12:26 +0900

    q

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/image',imageRoute)
 app.use('/api/location' , locationRoute)
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
 
